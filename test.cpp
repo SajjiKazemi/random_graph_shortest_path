793e6cfbecfc425178f3972a1023abc481c478b6
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 13:13:17 -0500

    Update tests

--- test.cpp
+++ test.cpp
@@ -7,49 +7,52 @@
 #include "helpers.h"
 #include <unistd.h>
 #include <iostream>
+#include <fcntl.h>
 
 TEST_CASE("Check rgen") 
 {
-    std::vector<int> result = random_number::rgen();
-    CHECK(result.size() == 4);
-    CHECK(result[0] >= 2);
-    CHECK(result[0] <= 10);
-    CHECK(result[1] >= 1);
-    CHECK(result[1] <= 5);
-    CHECK(result[2] >= 5);
-    CHECK(result[2] <= 5);
-    CHECK(result[3] >= -20);
-    CHECK(result[3] <= 20);
-
-    result = random_number::rgen(100, 10, 10, 100);
-    CHECK(result.size() == 4);
-    CHECK(result[0] >= 2);
-    CHECK(result[0] <= 100);
-    CHECK(result[1] >= 1);
-    CHECK(result[1] <= 10);
-    CHECK(result[2] >= 5);
-    CHECK(result[2] <= 10);
-    CHECK(result[3] >= -100);
-    CHECK(result[3] <= 100);
+    int random_number = rgen_needs::rgen(7,9);
+    CHECK(random_number >= 7);
+    CHECK(random_number <= 9);
 }
 
-std::string exec(const char* cmd) {
+std::string exec(const std::vector<std::string>& commands) {
     std::array<char, 128> buffer;
     std::string result;
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
-        result += buffer.data();
+
+    for (const auto& cmd : commands) {
+        FILE* pipe = popen(cmd.c_str(), "r");
+        if (!pipe) {
+            throw std::runtime_error("popen() failed for command: " + cmd);
+        }
+
+        while (fgets(buffer.data(), buffer.size(), pipe) != nullptr) {
+            result += buffer.data();
+        }
+
+        pclose(pipe);
     }
-    pclose(pipe);
+
     return result;
 }
 
 TEST_CASE("Test the rgen executable") 
 {
+    std::vector<std::string> commands;
     std::string command = "./rgen -s 15 -l -1 -n 21 2>&1";
-    std::string output = exec(command.c_str());
+    commands.push_back(command);
+    std::string output = exec(commands);
 
     // Use doctest assertions to check the output
     CHECK(output == "Error: Invalid option\n");
 }
+
+TEST_CASE("Test the pyfile executable") 
+ {
+    std::vector<std::string> commands;
+    std::string command = "make pyfile < ../input.txt";
+    commands.push_back(command);
+    std::string output = exec(commands);
+    CHECK(output == "Error: You did not obey the requested format");
+
+ }
