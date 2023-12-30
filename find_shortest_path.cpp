697ff78d119856c9355455ac8b68321acac9715f
sajji <sajjadkazemi.e@gmail.com>
2023-12-30 12:53:48 -0500

    Update the code with find_shortest_path code

--- find_shortest_path.cpp
+++ find_shortest_path.cpp
@@ -1,6 +1,22 @@
+// Compile with c++ ece650-a2cpp -std=c++11 -o ece650-a2
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "helpers.h"
+#include "MyGraph.h"
 
-int main(void) {
-    std::cout << "Replace with your solution for Assignment 2\n";
-    return 0;
+int main(int argc, char** argv) {
+
+    // read from stdin until EOF
+    MyGraph graph;
+    while (!std::cin.eof()) {
+
+        std::string line;
+        std::getline(std::cin, line);
+
+        // get the command from the line
+        std::string command = line_parser::get_command(line, graph);
+        line_parser::analyze_command(command, line, graph);
+    }
 }
